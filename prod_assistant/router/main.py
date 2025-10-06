7378cd1f291fc5699919855d3a431ad9f4f41395
SanjayNarendra <narendra.sanjay96@gmail.com>
2025-10-06 12:19:32 +0200

    docker file added

--- prod_assistant/router/main.py
+++ prod_assistant/router/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from prod_assistant.workflow.agentic_rag_workflow import AgenticRAG
+from prod_assistant.workflow.agentic_workflow_with_mcp_websearch import AgenticRAG
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
--- prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
+++ prod_assistant/workflow/agentic_workflow_with_mcp_websearch.py
@@ -3,15 +3,15 @@ from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-
-from prompt_library.prompts import PROMPT_REGISTRY, PromptType
-from retriever.retrieval import Retriever
-from utils.model_loader import ModelLoader
 from langgraph.checkpoint.memory import MemorySaver
-import asyncio
-from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
+from langgraph.graph.message import add_messages
 from langchain_mcp_adapters.client import MultiServerMCPClient
+import asyncio
+
+from prod_assistant.prompt_library.prompts import PROMPT_REGISTRY, PromptType
+from prod_assistant.retriever.retrieval import Retriever
+from prod_assistant.utils.model_loader import ModelLoader
+from prod_assistant.evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 
 
 class AgenticRAG:
